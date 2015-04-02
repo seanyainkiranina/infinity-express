d271d120828e8428a0970fa35cc7505546654955
seanyainkiranina <seanyainkiranina@yahoo.com>
2015-04-01 23:57:21 -0500

    real autoloader

--- html/autoloader.php
+++ html/autoloader.php
@@ -1,9 +1,8 @@
 <?php
 
+
 $dirs=array("../lib/",
-		"../config/",
-		"../views/",
-		"../models/");
+		"../config/");
 
 foreach($dirs as $dir){
 
@@ -20,6 +19,28 @@ if (is_dir($dir)){
 }
 }
 
+	if(!function_exists('classAutoLoader')){
+
+		function classAutoLoader($class){
+
+			$dirs=array("../views/",
+				"../models/");
+
+				foreach($dirs as $dir){
+
+					if (is_dir($dir)){
+					if (is_file($dir.$class.".php")){
+					require_once($dir.$class.".php");
+
+					}
+					}
+				}
+
+			}
+	}
+	spl_autoload_register('classAutoLoader');
+
   require_once 'vendor/autoload.php';
   require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+
 
