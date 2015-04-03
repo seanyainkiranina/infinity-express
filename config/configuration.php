ab531d1d8054c219dcf1826cf929b5ea97d9c0f0
seanyainkiranina <seanyainkiranina@yahoo.com>
2015-04-02 20:20:27 -0500

    Added Angular

--- config/configuration.php
+++ config/configuration.php
@@ -3,7 +3,6 @@
 /*
  * This file is part of Infinity  Express
  *
- * Copyright (c) 2014 
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
